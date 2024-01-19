bce2179abbf76065faa20aa1ee3bd2ee9d1c1e34
GMPythonGit <k-miura@bd5.so-net.ne.jp>
2024-01-20 06:37:55 +0900

    add and change from Home

---
+++ Introduction_to_Programing_1/ITP1_1_A_Hello World.py
@@ -0,0 +1,9 @@
+## ITP1_1_A_Hello World.py: Coded by Kinya MIURA, 240119
+
+print('Hello World')
+
+
+'''
+
+'''
+
---
+++ Introduction_to_Programing_1/ITP1_1_B_X_Cube.py
@@ -0,0 +1,11 @@
+## ITP1_1_A_Hello World.py: Coded by Kinya MIURA, 240119
+
+xx, = map(int, input().split())
+
+print(xx ** 3)
+
+
+'''
+
+'''
+
